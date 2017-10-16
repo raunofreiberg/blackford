7c4c5c1650d69451d43e0e914c11c8656c223451
Rauno <rauno@thorgate.eu>
2017-10-16 21:44:24 +0300

    Tests init

---
+++ client/tests/Todos.test.js
@@ -0,0 +1,3 @@
+test('asd', () => {
+    expect(1).toBe(1);
+});
---
+++ client/tests/setup.js
@@ -0,0 +1,6 @@
+import { shallow, render, mount } from 'enzyme';
+
+// Make Enzyme functions available in all test files without importing
+global.shallow = shallow;
+global.render = render;
+global.mount = mount;

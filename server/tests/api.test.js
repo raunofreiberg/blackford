7612aadceb839d46b7065e8a884e1aaca7211166
Rauno <rauno@thorgate.eu>
2017-11-19 06:49:50 +0200

    Faulty JWT test

--- server/tests/api.test.js
+++ server/tests/api.test.js
@@ -1,4 +1,6 @@
 const request = require('supertest');
+const jwt = require('jsonwebtoken');
+
 const knex = require('../dbConnect');
 const app = require('../server');
 const { encodeToken } = require('../auth/utils');
@@ -23,7 +25,7 @@ describe('# Authentication', () => {
             .expect(403, done);
     });
 
-    it('GET /api/posts with a JWT', async (done) => {
+    it('GET /api/posts with a valid JWT', async (done) => {
         const user = await knex('users').first();
         const token = encodeToken(user);
 
@@ -32,5 +34,28 @@ describe('# Authentication', () => {
             .set('Authorization', `Bearer ${token}`)
             .expect('Content-type', /json/)
             .expect(200, done);
+    });
+
+    it('GET /api/posts with a invalid JWT', async (done) => {
+        const user = await knex('users').first();
+        const encodeFaultyJwt = ({ id, username, avatar }) => {
+            const payload = {
+                id,
+                username,
+                avatar,
+            };
+
+            return jwt.sign(payload, 'wrongSecretToken', {
+                expiresIn: '14 days',
+            });
+        };
+
+        const token = encodeFaultyJwt(user);
+        
+        request(app)
+            .get('/api/posts')
+            .set('Authorization', `Bearer ${token}`)
+            .expect('Content-type', /json/)
+            .expect(401, done);
     });
 });

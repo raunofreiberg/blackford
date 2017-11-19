dfd64fcb01761c758456623ed82e3b66ee5fbfc8
Rauno <rauno@thorgate.eu>
2017-11-19 06:57:33 +0200

    ci ..

--- server/tests/api.test.js
+++ server/tests/api.test.js
@@ -21,6 +21,7 @@ describe('# Authentication', () => {
                     console.log(err)
                 } else {
                     console.log(res);
+                    done()
                 }
             })
     });
